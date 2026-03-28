4ec6c555dac2407f6d8663d4fbe1f391cefd05fa
Dénes Gáspár <gdenes007@gmail.com>
2026-03-28 21:11:30 +0100

    added README file and completed the task

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿System.Console.Write("A szóközzel elválasztott karaktersor: ");
+string [] betuk = Console.ReadLine().Split(' ');
+int[] kodok=new int[betuk.Length];
+System.Console.WriteLine("A karakterek kódjai: ");
+for (int i = 0; i < betuk.Length; i++)
+{
+    int kod=Convert.ToChar(betuk[i]);
+    kodok[i]=kod;
+    System.Console.Write($"{kodok[i]} ");
+}
+Console.ReadKey();
---
+++ README.md
@@ -0,0 +1,2 @@
+Kódoló
+Feladatleírás: Kérjen be a felhasználótól egy szóközökkel elválasztott karaktersorozatot! Hozza létre és írja ki azt a sorozatot, amely a karaktersorozatban lévő karakterek kódjait tárolja!
